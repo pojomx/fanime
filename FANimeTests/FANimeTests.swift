2e478972b3ec167cc10e4aedb2878cb37450459b
Alan Milke <alan.marquez@rollins.com>
2025-04-04 09:06:41 -0600

    Initial Commit

---
+++ FANimeTests/FANimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FANimeTests.swift
+//  FANimeTests
+//
+//  Created by Alan Milke on 04/04/25.
+//
+
+import Testing
+@testable import FANime
+
+struct FANimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
